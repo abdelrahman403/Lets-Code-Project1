8bf3d7831a26ebe8d93b8be827dfe62406265af0
abdelrhman-abdelazim <abdelrhmanhassan882@gmail.com>
2018-01-31 11:18:13 +0200

    all component of navbar are finished with responsive

--- js/main.js
+++ js/main.js
@@ -1,2 +1,44 @@
-console.log("hello world");
-console.log("Hi from repo");
+$(document).ready(function(){
+    var clicked = false;
+    var $header = $('header');
+    $header.find('nav .nav-btn').on('click', function(){
+        var $this = $(this);
+        if(!clicked) {
+            clicked = true;
+            $this.parent().toggleClass('active');
+            setTimeout(function(){
+                clicked = false;
+            }, 1250);
+        }
+    });
+
+    var clickOver = null,
+        $nav = $('nav');
+    $(document).on('click', function(e){
+        clickOver = $(e.target);
+        if(!clickOver.closest('header').length && $nav.hasClass('active')){
+            if(!clicked){
+                clicked = true;
+                $nav.removeClass('active');
+                setTimeout(function(){
+                    clicked = false;
+                }, 1250);
+            }
+        }
+    });
+
+    var $mainSection = $('.main-section');
+    $(window).scroll(function(){
+        if($(window).scrollTop() >= $mainSection.height() - 100){
+            $header.addClass('sticky');
+        } else {
+            $header.removeClass('sticky');
+        }
+    });
+
+});
+
+
+
+
+
