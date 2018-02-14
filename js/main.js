e6d1a3877b24fafbc2d17a500fd8cecf43aa10e9
abdelrhman-abdelazim <abdelrhmanhassan882@gmail.com>
2018-02-15 00:03:01 +0200

    Finishing menu slider script

--- js/main.js
+++ js/main.js
@@ -36,6 +36,118 @@ $(document).ready(function(){
         }
     });
 
+    function menuSlider(){
+        var $sliderContainer = $('.menu-section .slider-container'),
+            $slider = $sliderContainer.find('.slider'),
+            $sliderBanner = $slider.find('.slider-banner'),
+            $sliderItems = $sliderBanner.find('.slider-item'),
+            itemsLength = $sliderItems.length,
+            $nextArrow = $sliderContainer.find('.slider-arrows .arrow.next'),
+            $prevArrow = $sliderContainer.find('.slider-arrows .arrow.prev');
+
+            var itemWidth;
+            
+            var slidesToShow = 3,
+                activeSlides = slidesToShow,
+                itemMove = 0,
+                clicked = false;
+                
+            function responsiveSlides() {
+                if($(window).width() <= 991 && $(window).width() > 550){
+                    slidesToShow = 2;
+                    $sliderBanner.css('left', '0');
+                    itemMove = 0;
+                    $nextArrow.removeClass('disabled');
+                    $prevArrow.addClass('disabled');
+                }
+                else if($(window).width() <= 550) {
+                    slidesToShow = 1;
+                    $sliderBanner.css('left', '0');
+                    itemMove = 0;
+                    $nextArrow.removeClass('disabled');
+                    $prevArrow.addClass('disabled');
+                }
+                else {
+                    slidesToShow = 3;
+                    $sliderBanner.css('left', '0');
+                    itemMove = 0;
+                    $nextArrow.removeClass('disabled');
+                    $prevArrow.addClass('disabled');
+                }
+            } responsiveSlides();
+
+            activeSlides = slidesToShow;
+            
+            $sliderItems.outerWidth(parseInt($slider.width() / slidesToShow) + 10);
+
+            function fixWidth() {
+                itemWidth = $sliderItems.outerWidth();
+                $sliderBanner.width(itemWidth * itemsLength);
+            } fixWidth();
+
+
+            $(window).on('resize', () => {
+                responsiveSlides();
+                activeSlides = slidesToShow;
+
+                if($(window).width() <= 550) {
+                    $sliderItems.outerWidth(parseInt($slider.width() / slidesToShow) + 30);
+                } else {
+                    $sliderItems.outerWidth(parseInt($slider.width() / slidesToShow) + 10);
+                }
+
+                fixWidth();
+            });
+            
+            function checkStatus() {
+                if(activeSlides === itemsLength)
+                    $nextArrow.addClass('disabled');
+                else
+                    $nextArrow.removeClass('disabled');
+                
+                if(activeSlides === slidesToShow)
+                    $prevArrow.addClass('disabled');
+                else
+                    $prevArrow.removeClass('disabled');
+            } checkStatus();
+
+            function leftCalc() {
+                return itemWidth * itemMove;
+            }
+            
+            $nextArrow.on('click', () => {
+                if(!clicked) {
+                    if(activeSlides != itemsLength) {
+                        clicked = true;
+                        itemMove++;
+                        $sliderBanner.css('left', -leftCalc());
+                        activeSlides++;
+                        setTimeout(function(){
+                            clicked = false;
+                        }, 450);
+                        checkStatus();
+                    }
+                }
+            });
+            
+            $prevArrow.on('click', () => {
+                if(!clicked) {
+                    if(activeSlides > slidesToShow) {
+                        clicked = true;
+                        itemMove--;
+                        $sliderBanner.css('left', -leftCalc());
+                        activeSlides--;
+                        setTimeout(function(){
+                            clicked = false;
+                        }, 450);
+                        checkStatus();
+                    }
+                }
+            });
+            
+    } menuSlider();
+
+
 });
 
 
